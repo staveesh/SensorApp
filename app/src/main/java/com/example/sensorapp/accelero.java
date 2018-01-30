d0a2541236ff91988846dd4d98dd38e5d82aa9ac
staveesh <sharmataveesh@yahoo.in>
2018-01-30 18:30:50 +0530

    Added rock-paper-scissor

--- app/src/main/java/com/example/sensorapp/accelero.java
+++ app/src/main/java/com/example/sensorapp/accelero.java
@@ -1,13 +1,77 @@
 package com.example.sensorapp;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.Image;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
-public class accelero extends AppCompatActivity {
+import java.util.Random;
+
+public class accelero extends AppCompatActivity{
+
+    private ImageView iv;
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelero);
+
+        iv = (ImageView) findViewById(R.id.rps);
+        // ShakeDetector initialization
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
+            @Override
+            public void onShake(int count) {
+				/*
+				 * The following method, "handleShakeEvent(count):" is a stub //
+				 * method you would use to setup whatever you want done once the
+				 * device has been shook.
+				 */
+                handleShakeEvent(count);
+                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                // Vibrate for 500 milliseconds
+                v.vibrate(500);
+            }
+        });
     }
+
+    protected void handleShakeEvent(int count){
+        Random rand  = new Random();
+        int randNo = rand.nextInt(3)+1;
+        if(randNo == 1)
+            iv.setImageResource(R.drawable.rock);
+        else if(randNo == 2)
+            iv.setImageResource(R.drawable.paper);
+        else
+            iv.setImageResource(R.drawable.scissor);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
+    }
+
 }
