5f3b2ed948740177a475a6e1485fa30d92a96d49
staveesh <sharmataveesh@yahoo.in>
2018-01-30 17:43:22 +0530

    Implemented proximity sensor

--- app/src/main/java/com/example/sensorapp/prox.java
+++ app/src/main/java/com/example/sensorapp/prox.java
@@ -1,13 +1,49 @@
 package com.example.sensorapp;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class prox extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class prox extends AppCompatActivity implements SensorEventListener{
+    private SensorManager sensorManager;
+    private Sensor mSensor;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prox);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        if(mSensor == null){
+            Toast.makeText(this,"Proximity sensor is not available", Toast.LENGTH_LONG).show();
+            finish();
+        }
+        else{
+            sensorManager.registerListener(this,mSensor,SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.values[0] < mSensor.getMaximumRange()){
+            getWindow().getDecorView().setBackgroundColor(Color.RED);
+        }
+        else{
+            getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
